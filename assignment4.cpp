2a4eed44469f10a07cddf917282d565d15fdcd55
Hemayatullah Sangin <hemayat.sangin@gmail.com>
2023-05-12 15:52:59 +0300

    First commit
    I will be using C++ for this assignment.

---
+++ assignment4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	
+	int number;
+	cout<<"Enter your number to check!"<<endl;
+	cin>>number;
+	
+	
+	cout<<"Your number is prime:"<<number<<endl;
+	
+	
+	return 0;
+}
