115c675730d855fb44f33ca71ae291acbb6ce35d
Hemayatullah Sangin <hemayat.sangin@gmail.com>
2023-05-14 12:16:30 +0300

    CheckingForPrimeNumber.
    This is the first logic for finding the prime and composite.

--- assignment4.cpp
+++ assignment4.cpp
@@ -1,16 +1,39 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
 
-int main(){
+
+int main() {
+  int number;
+
+  cout << "Enter number to check for prime | Composite: ";
+  cin >> number;
+
+ if (number > 1) {
+  
+   for (int i = 2; i <= number / 2; i++) {
+    if (number % i == 0) {
+    cout<<number <<" Is composite number."<<endl;
+
+    }
+    
+    else{
+    	 cout<<number <<" Is prime number."<<endl;
+	}
 	
-	int number;
-	cout<<"Enter your number to check!"<<endl;
-	cin>>number;
-	
-	
-	cout<<"Your number is prime:"<<number<<endl;
-	
-	
-	return 0;
+	  break;
+  }
+  }
+  
+  else{
+    cout<<number <<" Is composite number."<<endl;
+  }
+  
+  
+
+
+
+
+  return 0;
 }
