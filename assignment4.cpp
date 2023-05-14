705b5a6287a88173049945a086f485b20a331987
Hemayatullah Sangin <hemayat.sangin@gmail.com>
2023-05-14 14:16:44 +0300

    Improved the logic due to errors.
    The previous logic was not finding some numbers correctly.

--- assignment4.cpp
+++ assignment4.cpp
@@ -7,32 +7,28 @@ using namespace std;
 int main() {
   int number;
 
+ bool check_prime = true;
+
   cout << "Enter number to check for prime | Composite: ";
   cin >> number;
 
- if (number > 1) {
-  
-   for (int i = 2; i <= number / 2; i++) {
+
+  if (number <= 1) {
+    check_prime = false;
+  }
+
+  for (int i = 2; i <= number / 2; i++) {
     if (number % i == 0) {
-    cout<<number <<" Is composite number."<<endl;
-
+      check_prime = false;
+      break;
     }
-    
-    else{
-    	 cout<<number <<" Is prime number."<<endl;
-	}
-	
-	  break;
   }
-  }
-  
-  else{
-    cout<<number <<" Is composite number."<<endl;
-  }
-  
-  
 
-
+  if (check_prime) {
+    cout << number << " is a prime number." << endl;
+  } else {
+    cout << number << " is a composite number." << endl;
+  }
 
 
   return 0;
