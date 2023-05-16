03904c62f11c5eda48847f8bd9bcf3673024858b
Hemayatullah Sangin <hemayat.sangin@gmail.com>
2023-05-16 20:59:50 +0300

    Finding the factors.
    Finding the factors of composite number.

--- assignment4.cpp
+++ assignment4.cpp
@@ -2,16 +2,12 @@
 
 using namespace std;
 
-
-
 int main() {
   int number;
+  bool check_prime = true;
 
- bool check_prime = true;
-
-  cout << "Enter number to check for prime | Composite: ";
+  cout << "Please give a number-> ";
   cin >> number;
-
 
   if (number <= 1) {
     check_prime = false;
@@ -25,11 +21,17 @@ int main() {
   }
 
   if (check_prime) {
-    cout << number << " is a prime number." << endl;
+    cout << number << " is a prime number and factors are ->" << endl;
   } else {
-    cout << number << " is a composite number." << endl;
-  }
+    cout << number << " is composite number and factors are -> ";
 
+    for (int i = 1; i <= number; i++) {
+      if (number % i == 0) {
+        cout << i << ",";
+      }
+    }
+    cout << endl;
+  }
 
   return 0;
 }
